a4ce2642c28904a72d1d0b45f9c1e82ef55d7d39
Vignesh  R <vigneshr@web-3.in>
2022-11-14 17:24:03 +0530

    This is new class added as Vicky dev

--- student.java
+++ student.java
@@ -3,3 +3,9 @@ class Main {
         System.out.println("Hi, Welcome to GitHub");
     }
 }
+
+class Vicky {
+    public static void main() {
+        System.out.println("Welcome java devloper - Vicky");
+    }
+}
